390750683982aba31f44c54172f19366d917c92e
jsjchai <jsjchai@163.com>
2020-05-14 15:06:43 +0800

    单向链表

--- linear-list/link/LinkList.go
+++ linear-list/link/LinkList.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 const (
@@ -28,7 +29,7 @@ func (list *LinkList) get(i int, e *ElemType) string {
 	for j = 1; j < i; j++ {
 		p = p.next
 	}
-	if p == nil || j > 1 {
+	if p == nil || j > i {
 		return ERROR
 	}
 	*e = p.data
@@ -71,6 +72,31 @@ func (list *LinkList) delete(i int, e *ElemType) string {
 
 }
 
+func (list *LinkList) createListHead(n int) {
+	var p *Node
+	for i := 0; i < n; i++ {
+		var num = ElemType(rand.Intn(100) + 1)
+		fmt.Println(num)
+		p = &Node{data: num, next: list.head.next}
+		list.head.next = p
+		list.length++
+	}
+}
+
+func (list *LinkList) createListTail(n int) {
+	var p, r *Node
+	r = list.head
+	for i := 0; i < n; i++ {
+		var num = ElemType(rand.Intn(100) + 1)
+		fmt.Println(num)
+		p = &Node{data: num, next: nil}
+		r.next = p
+		r = p
+		list.length++
+	}
+	r.next = nil
+}
+
 func (list *LinkList) println() {
 	if list.head == nil || list.length == 0 {
 		return
@@ -85,7 +111,12 @@ func (list *LinkList) println() {
 
 func main() {
 
-	list := LinkList{head: &Node{data: 0, next: nil}, length: 1}
+	list := LinkList{head: &Node{data: -1, next: nil}, length: 1}
+	//list.createListHead(10)
+	list.createListTail(10)
+	fmt.Println("------------")
+	list.println()
+
 	list.insert(1, 6)
 	list.insert(2, 7)
 	list.insert(3, 8)
@@ -93,5 +124,9 @@ func main() {
 	list.println()
 	var e ElemType
 	fmt.Println("delete", list.delete(1, &e))
+	fmt.Println("e = ", e)
 	list.println()
+	fmt.Println("get", list.get(2, &e))
+	fmt.Println("e = ", e)
+
 }
