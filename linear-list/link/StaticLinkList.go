96e2428d5472e3b374db7f69a7d77652f744ef32
jsjchai <jsjchai@163.com>
2020-05-15 11:00:03 +0800

    静态链表

--- linear-list/link/StaticLinkList.go
+++ linear-list/link/StaticLinkList.go
@@ -1,6 +1,9 @@
 package main
 
-const MAX_SIZE = 10
+import (
+	"fmt"
+	"os"
+)
 
 type StaticLinkList struct {
 	data   int
@@ -11,18 +14,49 @@ type StaticLinkList struct {
 func initList(size int) []StaticLinkList {
 
 	list := make([]StaticLinkList, size)
-	for i := 0; i < MAX_SIZE-1; i++ {
+	for i := 0; i < size-1; i++ {
 		list[i].cursor = i + 1
 	}
-	list[MAX_SIZE-1].cursor = 0
+	list[size-1].cursor = 0
 	return list
 }
 
 /*若备用空间链表非空，则返回分配的结点下标,否则返回0*/
 func mallocSLL(list []StaticLinkList) int {
 	i := list[0].cursor
-	if list[0].cursor >= 0 {
-		list[0].cursor = list[i].cursor
+	if i == 0 {
+		os.Exit(0)
 	}
+	list[0].cursor = list[i].cursor
 	return i
+}
+func insert(list []StaticLinkList, i int, data int) string {
+	var j, k, l int
+	k = len(list) - 1
+	if i < 1 || i > len(list)+1 {
+		return "error"
+	}
+	j = mallocSLL(list)
+	if j > 0 {
+		list[j].data = data
+		for l = 1; l <= i-1; l++ {
+			k = list[k].cursor
+		}
+		list[j].cursor = list[k].cursor
+		list[k].cursor = j
+		return "ok"
+	}
+	return "error"
+}
+func main() {
+	list := initList(5)
+	insert(list, 1, 11)
+	insert(list, 1, 22)
+	insert(list, 1, 33)
+	insert(list, 1, 44)
+
+	for i, arr := range list {
+		fmt.Println("i=", i, "cursor=", arr.cursor, "data=", arr.data)
+	}
+
 }
