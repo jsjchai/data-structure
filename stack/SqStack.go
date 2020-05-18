52aece6074638e290c54884acedd216f4fe92ca7
jsjchai <jsjchai@163.com>
2020-05-18 15:27:20 +0800

    栈

--- stack/SqStack.go
+++ stack/SqStack.go
@@ -1,5 +1,47 @@
 package main
 
+import "fmt"
+
+const MAXSIZE int = 5
+
+type SqStack struct {
+	data []int
+	top  int //用于栈顶指针
+}
+
+func (stack *SqStack) push(e int) string {
+	if stack.top == MAXSIZE-1 {
+		return "ERROR"
+	}
+	stack.top++
+	stack.data[stack.top] = e
+	return "OK"
+}
+
+func (stack *SqStack) pop(e *int) string {
+	if stack.top == -1 {
+		return "ERROR"
+	}
+	*e = stack.data[stack.top]
+	stack.top--
+	return "OK"
+}
+
 func main() {
+
+	stack := SqStack{data: make([]int, MAXSIZE), top: -1}
+	stack.push(5)
+	stack.push(4)
+	stack.push(3)
+	stack.push(2)
+	stack.push(1)
+
+	fmt.Println("出栈：")
+
+	var e int
+	for stack.top >= 0 {
+		stack.pop(&e)
+		fmt.Print(e, " ")
+	}
 
 }
